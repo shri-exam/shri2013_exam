bf081dbee6386fb9e49a853b3fffb31fbf320bd8
Ruslan Abdullaev <skarlatin@mail.ru>
2013-03-25 23:05:13 +0200

    Выводим/убираем стрелочки по mouseenter/mouseleave на window, правда пока без анимации

--- j/main.js
+++ j/main.js
@@ -7,23 +7,18 @@ var $slideshow = $('.b-slideshow');
 $slideshow.html('<img class="b-slideshow__image" src="" alt="" />');
 var $slideshowImg = $('.b-slideshow__image');
 
-$.getJSON(url, function(root){
-		  image = root.entries;
+//Yandex API
+  $.getJSON(url, function(root) {
+     image = root.entries;
   }).done(function() {
-    loadImg(id,'XL');
-
+    loadImg(id,"XL");
+  
     var inner = '';
       $.each(image,function (i, val) {
           inner += '<img src="' + image[i].img.XXS.href + '" alt="" />';
       });
       $('.b-gallery').append(inner);
   });
-
-$slideshowImg.click(function() {
-      var id = $slideshowImg.attr("data-photo-id");
-      loadImg(++id,"XL");
-  });
-
 
 //Загружаем изображение по id    
 function loadImg(id,size) {
@@ -33,11 +28,35 @@ function loadImg(id,size) {
 
     $slideshowImg.load(function () {
         dfd.resolve();
+        resizeimage();
     }).attr({'src': src, 'data-photo-id': id, 'alt': alt});
     return dfd.promise();
 };
 
+//масштабируем под размеры viewport и центруем
+function resizeimage() {
+        var vpHeight = $(window).height(), vpWidth  = $(window).width();
+        var zoomHeight = $slideshowImg.height() > vpHeight? vpHeight : $slideshowImg.height();
+        var zoomWidth = $slideshowImg.width() > vpWidth  ? vpWidth  : $slideshowImg.width();
+        $slideshowImg.attr({'height':zoomHeight});
+        $slideshow.css({'top': (vpHeight - zoomHeight)/2 +'px', 'left': (vpWidth - zoomWidth)/2 +'px'});
+}
 
+$('.b-right-arrow').on("click", function() {
+      var id = $slideshowImg.attr("data-photo-id");
+      loadImg(++id,"XL");
+  });
 
+$(window).resize(function(){
+    resizeimage();
+})
+.mouseenter(function(){
+    if(id>1) $('.b-left-arrow').show();
+    $('.b-right-arrow').show();
+})
+.mouseleave(function(){
+    $('.b-left-arrow').hide();
+    $('.b-right-arrow').hide();
+});
 
 })(jQuery);
