a9bfa4184dbda682c76ed497ad8f73d7b7bf3b5e
Ruslan Abdullaev <skarlatin@mail.ru>
2013-03-30 14:51:10 +0200

    Исправил рамочку в галереи, добавил учет границ галереи при выводе, загружаемая картинка выбирается в зависимости от разрешения экрана

--- j/main.js
+++ j/main.js
@@ -1,32 +1,40 @@
 (function($) {
 
 //Init variables
-      var id = 0, image ='', size = '';
+      var id = 0, image ='', size = '', last_image;
       var url = 'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?format=json&callback=?';
       var screen_height = window.screen.availHeight;
       var size_suffix = {'XL': '800', 'L': '500', 'M':'300', 'S':'150', 'XS': '100', 'XXS': '75', 'XXXS': '50'};
       var $slideshow = $('.b-slideshow'), $gallery = $('.b-gallery'), $gallery_wrapper = $('.b-gallery__wrapper');
       var $left_arrow = $('.b-left-arrow'), $right_arrow = $('.b-right-arrow');
-      var $last_image = '', $slideshow_image = '';
+      var $selected_image = '', $slideshow_image = '';
+//
+      $.extend($.easing,
+      {
+            def: 'swing',
+            easeOutCubic: function (x, t, b, c, d) {
+                return c*((t=t/d-1)*t*t + 1) + b;
+            }
+      });
 
 //Yandex API
       function loadGallery(url) {
             $.getJSON(url, function(root) {
                   image = root.entries;
             }).done(function() {
-                  loadImg(id,"XL");
-            
               var inner = '';
                 $.each(image,function (i, val) {
                   inner += '<a href="#"><img class="b-gallery__image" src="' + image[i].img.XXS.href + '" alt="" /></a>';
                 });
-                  $gallery_wrapper.append(inner);
+
+                  last_image = image.length - 1;
+                  $gallery_wrapper.append(inner).css('width', last_image*96);
+                  loadImg(id, size);
             });
       };
 
 //Загружаем изображение по id    
       function loadImg(id,size) {
-            var size = size || 'XL';
             var dfd = $.Deferred();
             var src = image[id].img[size].href;
             var alt = image[id].title;
@@ -34,65 +42,57 @@
             $slideshow.hide().html(container);
             $slideshow_image = $('.b-slideshow__image');
 
-            $('.b-slideshow__image').load(function () {
+            $slideshow_image.load(function () {
                 dfd.resolve();
-                $slideshow.show();
+                $slideshow.fadeIn();
                 resizeimage();
             });
-                $last_image = $('.b-gallery__image').eq(id);
-                $last_image.addClass('b-gallery__image_active');
+
+                $selected_image = $('.b-gallery__image').eq(id);
+                $selected_image.addClass('b-gallery__image_active');
                 return dfd.promise();
       };
 
 //масштабируем под размеры viewport и центруем
       function resizeimage() {
             var vp_height = $(window).height(), vp_width  = $(window).width();
-            var zoom_height =  vp_height - 300;
+            var zoom_height =  vp_height - 250;
             $slideshow_image.attr({'height':zoom_height});
             var zoom_width = $slideshow_image.width() > vp_width  ? vp_width : $slideshow_image.width();
             $slideshow.css({'top': (vp_height - zoom_height)/2 +'px', 'left': (vp_width - zoom_width)/2 +'px'});
       }
 //
       function showbutton(obj) {
-            obj.show();
+            obj.css({'top': ($(window).height() - obj.height())/2 +'px'})
+            .show();
       };
 //
       function calcImageSize(size_suffix, screen_height) {
-            size = $.grep(size_suffix, function(el,idx){
-                if(size_suffix[el] < screen_height) return true;
-                else return false
+            var calc_size = $.map(size_suffix, function(value,index){
+                return (value > screen_height) ? index : null; 
             });
-            console.log(size);
+            size = calc_size != "" ? calc_size : "XL";
       };
-
-      $.extend($.easing,
-      {
-            def: 'swing',
-            easeOutCubic: function (x, t, b, c, d) {
-                return c*((t=t/d-1)*t*t + 1) + b;
-            }
-
-      });
 
 //Обработчики событий
       $left_arrow.click(function() {
-            $last_image.removeClass('b-gallery__image_active');
+            $selected_image.removeClass('b-gallery__image_active');
             if (id > 0 ) --id;
-            loadImg(id);
+            loadImg(id,size);
       });
 //
       $right_arrow.click(function() {
-            $last_image.removeClass('b-gallery__image_active');
-            ++id;
-            loadImg(id);
+            $selected_image.removeClass('b-gallery__image_active');
+            if (id < last_image-1) ++id;
+            loadImg(id,size);
       });
 //
       $(window).resize(function(){
-          resizeimage();
+            resizeimage();
       })
       .mouseenter(function(){
             if (id > 0)  showbutton($left_arrow);
-            showbutton($right_arrow);
+            if (id < last_image-1) showbutton($right_arrow);
       })
       .mouseleave(function(){
             $left_arrow.hide();
@@ -105,23 +105,22 @@
       });
 //
       $gallery.delegate('a', 'click', function(obj){
+            $selected_image.removeClass('b-gallery__image_active');
             id = $(this).index();
-            $('.b-gallery__image').eq(id).removeClass('b-gallery__image_active');
-            loadImg(id);
+            loadImg(id,size);
             return false;
       });
 //
-      $gallery.bind('mousewheel', function(event, delta, deltaX, deltaY) {
-            var offset = $gallery_wrapper.css('left').replace('px','');
-            var off = (offset == 0 && delta < 0) ?  offset + 'px' : '+=' + delta * 101; 
-            //var off = (offset < -5100 && delta < 0) ? offset + 'px' : '+=' + delta * 101;
-            console.log(offset);
-            console.log(off);
-            $('.b-gallery__wrapper').animate({'left': off}, 50, 'easeOutCubic');
+      $gallery.bind('mousewheel', function(event, delta) {
+        var visible_offset = $gallery_wrapper.width() - $gallery.width();
+            $gallery_wrapper.animate({'left': '+=' + delta * 96}, { duration: 75, easing: 'easeOutCubic', step: function(now, obj){
+                if (obj.end > 0) obj.end = '0';
+                else if (obj.end  < -visible_offset) obj.end = obj.start;
+            }});
       });
 
 //
-      loadGallery(url);
       calcImageSize(size_suffix, screen_height);
+      loadGallery(url);
 
 })(jQuery);
